9b1c6e31a95efdc60fde1a3c2f61111fd6535caa
Soichiro Yoshimura <yoshimura@soichiro.org>
2014-03-24 17:21:49 +0900

    トランザクションスクリプトの例を実装

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ name := "oopdb"
 version := "1.0"
 
 libraryDependencies ++= Seq(
+  "com.h2database" % "h2" % "1.3.175",
   "com.typesafe.slick" %% "slick" % "2.0.1",
   "org.slf4j" % "slf4j-nop" % "1.6.4"
 )
--- src/main/scala/TransactionScriptExample.scala
+++ src/main/scala/TransactionScriptExample.scala
@@ -1,14 +1,59 @@
+import scala.slick.driver.H2Driver.simple._
+import scala.slick.jdbc.{GetResult, StaticQuery => Q}
+
 object TransactionScriptExample {
   def main(args: Array[String]) {
-
-    println("こんにちわ")
+    SpotFinder.findSpot("アプカレの卵").foreach(println)
   }
 
-  trait Item {val id: Int;val name: String;val requireLevel: Int;}
-  case class DropItem(id: Int, name: String, requireLevel: Int, enemyName: String) extends Item
-  case class GatheringItem(id: Int, name: String, requireLevel: Int, requireGathererClass: String) extends Item
   case class Spot(id: Int, name: String, x: Int, y: Int)
 
-
+  object SpotFinder {
+    def findSpot(itemName: String): Seq[Spot] = {
+      Database.forURL("jdbc:h2:itemdb", driver = "org.h2.Driver") withSession {
+        implicit session =>
+          implicit val spotConverter = GetResult(r => Spot(r.<<, r.<<, r.<<, r.<<))
+          val queryForDrop = Q.query[String, Spot]( """
+        |select
+        |    spots.id,
+        |    spots.name,
+        |    spots.x,
+        |    spots.y
+        |from
+        |    (
+        |        drop_items
+        |        left join
+        |            drop_item_spot_relations
+        |        on  drop_items.id = drop_item_spot_relations.drop_item_id
+        |    )
+        |    left join
+        |        spots
+        |    on  drop_item_spot_relations.spot_id = spots.id
+        |where
+        |    drop_items.name = ?""".stripMargin)
+          val dropItems = queryForDrop.list(itemName)
+          val queryForGathering = Q.query[String, Spot]("""
+        |select
+        |    spots.id,
+        |    spots.name,
+        |    spots.x,
+        |    spots.y
+        |from
+        |    (
+        |        gathering_items
+        |        left join
+        |            gathering_item_spot_relations
+        |        on  gathering_items.id = gathering_item_spot_relations.gathering_item_id
+        |    )
+        |    left join
+        |        spots
+        |    on  gathering_item_spot_relations.spot_id = spots.id
+        |where
+        |    gathering_items.name = ?""".stripMargin)
+          val gatheringItems = queryForGathering.list(itemName)
+          dropItems ++ gatheringItems
+      }
+    }
+  }
 
 }
